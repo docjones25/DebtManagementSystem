cd00f3a05e2ff566848171e949881d3c81046db0
Neil Lawrence Peramo <peramo.383383@lipa.sti.edu.ph>
2026-04-29 19:56:37 +0800

    I did some rough designs in Customers form, it is only temporary and will be further improved

--- DebtManagementSystem/DebtManagementSystem/Customer.Designer.cs
+++ DebtManagementSystem/DebtManagementSystem/Customer.Designer.cs
@@ -28,12 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.addCus = new System.Windows.Forms.Button();
+            this.totDebt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addCus
+            // 
+            this.addCus.Location = new System.Drawing.Point(465, 19);
+            this.addCus.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.addCus.Name = "addCus";
+            this.addCus.Size = new System.Drawing.Size(96, 36);
+            this.addCus.TabIndex = 1;
+            this.addCus.Text = "Add Customer";
+            this.addCus.UseVisualStyleBackColor = true;
+            // 
+            // totDebt
+            // 
+            this.totDebt.Location = new System.Drawing.Point(41, 141);
+            this.totDebt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.totDebt.Name = "totDebt";
+            this.totDebt.Size = new System.Drawing.Size(520, 41);
+            this.totDebt.TabIndex = 2;
+            this.totDebt.Text = "Name                                          Contact No.                        " +
+    "                          Total Debt";
+            this.totDebt.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 126);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 126);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Contact";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(417, 126);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Total Debt";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(41, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(460, 30);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(507, 77);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(54, 30);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "🔍︎";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 231);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(520, 41);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Name                                          Contact No.                        " +
+    "                          Total Debt";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(41, 186);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(520, 41);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Name                                          Contact No.                        " +
+    "                          Total Debt";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(41, 276);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(520, 41);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Name                                          Contact No.                        " +
+    "                          Total Debt";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Customer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Customer";
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.totDebt);
+            this.Controls.Add(this.addCus);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Customer";
+            this.Text = "X";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button addCus;
+        private System.Windows.Forms.Button totDebt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
